f9b9702423f4b1ecfadc5bc3c7ca6257da4000db
Ann Dvorkina <anndvorkina@gmail.com>
2018-11-14 17:38:17 +0300

    Change api route

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   AUTH0_DOMAIN: 'nusha.eu.auth0.com', // e.g., kmaida.auth0.com
-  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+  AUTH0_API_AUDIENCE: 'https://go2goal.herokuapp.com:8083/api/', // e.g., 'http://localhost:8083/api/'
   MONGO_URI: process.env.MONGO_URI || 'mongodb://ann:1111@ds033126.mlab.com:33126/cardsauth',
   NAMESPACE: 'http://myapp.com/roles'
 };
